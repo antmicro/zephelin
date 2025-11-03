fa77c12b94a8b30bf04ab419aa25bce0e0cfe0dd
Maciej Torhan <mtorhan@antmicro.com>
2025-11-03 09:27:43 +0100

    [#85717] samples: common: tvm: Change sine model to one compiled for generic CPU
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- samples/common/tvm/src/sine.h
+++ samples/common/tvm/src/sine.h
@@ -5,25 +5,25 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-
 #ifndef SAMPLES_TRACE_TVM_PROFILER_SRC_SINE_H_
 #define SAMPLES_TRACE_TVM_PROFILER_SRC_SINE_H_
 
 #include <tvm/runtime/crt/module.h>
 #include <tvm/runtime/crt/packed_func.h>
 
-#define TVMGEN_QUANTIZED_FUNCTIONS(FUNC)		\
-	FUNC(tvmgen_quantized_fused_nn_dense_subtract_add_fixed_point_multiply_add_clip_cast) \
-	FUNC(tvmgen_quantized_fused_nn_dense_subtract_add_fixed_point_multiply_add_clip_cast_1) \
-	FUNC(tvmgen_quantized_fused_nn_dense_subtract_add_fixed_point_multiply_add_clip_cast_2) \
-	FUNC(tvmgen_quantized_fused_reshape) \
-	FUNC(tvmgen_quantized_fused_reshape_1)
+#define TVMGEN_QUANTIZED_FUNCTIONS(FUNC)                                                           \
+	FUNC(tvmgen_quantized_fused_nn_dense_add_fixed_point_multiply_add_clip_cast)               \
+	FUNC(tvmgen_quantized_fused_nn_dense_add_fixed_point_multiply_add_clip_cast_1)             \
+	FUNC(tvmgen_quantized_fused_nn_dense_add_fixed_point_multiply_add_clip_cast_2)             \
+	FUNC(tvmgen_quantized_fused_reshape_cast_subtract)                                         \
+	FUNC(tvmgen_quantized_fused_reshape_cast_subtract_1)
 
 #define TVMGEN_QUANTIZED_FUNCTIONS_COUNT "\x05"
 
-#define TVMGEN_QUANTIZED_DECLARE(func_name) \
-	extern int32_t func_name(void *args, int32_t *arg_type_ids, int32_t num_args, void *out_ret_value, \
-													 int32_t *out_ret_tcode, void *resource_handle);
+#define TVMGEN_QUANTIZED_DECLARE(func_name)                                                        \
+	extern int32_t func_name(void *args, int32_t *arg_type_ids, int32_t num_args,              \
+				 void *out_ret_value, int32_t *out_ret_tcode,                      \
+				 void *resource_handle);
 
 #define TVMGEN_QUANTIZED_FUNC_ARRAY(func_name) (TVMBackendPackedCFunc) func_name,
 
@@ -31,13 +31,19 @@
 
 TVMGEN_QUANTIZED_FUNCTIONS(TVMGEN_QUANTIZED_DECLARE)
 
-const TVMBackendPackedCFunc tvm_quantized_func_array[] = {TVMGEN_QUANTIZED_FUNCTIONS(TVMGEN_QUANTIZED_FUNC_ARRAY)};
+const TVMBackendPackedCFunc tvm_quantized_func_array[] = {
+	TVMGEN_QUANTIZED_FUNCTIONS(TVMGEN_QUANTIZED_FUNC_ARRAY)};
 
-const TVMFuncRegistry tvm_quantized_func_reg = {.names = TVMGEN_QUANTIZED_FUNCTIONS_COUNT TVMGEN_QUANTIZED_FUNCTIONS(TVMGEN_QUANTIZED_FUNC_REGISTRY) "\0",
+const TVMFuncRegistry tvm_quantized_func_reg = {
+	.names = TVMGEN_QUANTIZED_FUNCTIONS_COUNT TVMGEN_QUANTIZED_FUNCTIONS(
+		TVMGEN_QUANTIZED_FUNC_REGISTRY) "\0",
 	.funcs = tvm_quantized_func_array};
 
 const TVMModule g_tvm_quantized_module = {&tvm_quantized_func_reg};
 
-const TVMModule *TVMQuantizedLibEntryPoint(void) { return &g_tvm_quantized_module; }
+const TVMModule *TVMQuantizedLibEntryPoint(void)
+{
+	return &g_tvm_quantized_module;
+}
 
 #endif /* SAMPLES_TRACE_TVM_PROFILER_SRC_SINE_H_ */
