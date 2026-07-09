274daef9839e403edfc7d856e0c6a60065c9a9da
Mikolaj Klikowicz <mklikowicz@antmicro.com>
2026-07-09 10:30:41 +0200

    [#99020] Bump to 4.4.1
    Signed-off-by: Mikolaj Klikowicz <mklikowicz@antmicro.com>
    
    Signed-off-by: Jedrzej Czykier <jczykier@antmicro.com>

--- scripts/ci/install_deps.sh
+++ scripts/ci/install_deps.sh
@@ -7,25 +7,22 @@
 
 set -xeuo pipefail
 
-export RENODE_VERSION="renode-1.16.0+20250929gitc16006c94.linux-portable-dotnet.tar.gz"
+if [ -z "${RENODE_VERSION:-}" ]; then
+	echo "RENODE_VERSION is not set!"
+	exit 1
+fi
 
 # install deps with apt
 sudo apt update -qq
-sudo apt install -yqq --no-install-recommends gdb-multiarch mono-complete \
+sudo apt install -yqq --no-install-recommends gdb-multiarch \
   ccache dfu-util device-tree-compiler wget python3-dev python3-venv python3-tk \
   xz-utils file make gcc gcc-multilib g++-multilib libsdl2-dev libmagic1 \
   xxd git-lfs swig libelf-dev libdw-dev python3-packaging \
   libgtk2.0-0 screen uml-utilities libc6-dev libicu-dev \
-  python3 python3-pip git cmake ninja-build gperf unzip qemu-system-arm
-
-pip install --break-system-packages uv
-uv venv -p 3.11
-source .venv/bin/activate
-
-uv pip install --upgrade pip
+  python3 python3-pip git cmake ninja-build gperf unzip
 
 # install deps with pip
-uv pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # install recent flatbuffers-compiler
 FLATC_URL=https://github.com/google/flatbuffers/releases/download/v25.2.10/Linux.flatc.binary.g++-13.zip
--- scripts/ci/setup_zephyr.sh
+++ scripts/ci/setup_zephyr.sh
@@ -7,13 +7,8 @@
 
 set -xeuo pipefail
 
-wget -N --progress=dot:giga https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.17.2/zephyr-sdk-0.17.2_linux-x86_64.tar.xz -O zephyr-sdk.tar.gz && \
-    mkdir -p /opt/zephyr-sdk && \
-    tar -xf zephyr-sdk.tar.gz --strip 1 -C /opt/zephyr-sdk && \
-    rm zephyr-sdk.tar.gz
-
 west init -l .
 west update
-uv pip install -r ../zephyr/scripts/requirements.txt
 west patch apply
 west zephyr-export
+west packages pip --install --ignore-venv-check
