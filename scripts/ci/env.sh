f95dd5e745d9bb0cf5a3937f02ea297ba9e09734
Jan Bylicki <jbylicki@antmicro.com>
2026-07-17 14:25:03 +0200

    [wip] [#101086] .github: docs.yml: Change UV installation method
    Signed-off-by: Jan Bylicki <jbylicki@antmicro.com>

--- scripts/ci/env.sh
+++ scripts/ci/env.sh
@@ -1,4 +1,14 @@
 source .venv/bin/activate
-source $HOME/.local/bin/env
 export PATH=$PATH:/opt/renode
 source zpl_env.sh
+
+if [ -d renode_portable ]; then
+  export PYRENODE_PATH=$(realpath renode_portable)
+  export PYRENODE_BIN=$(realpath renode_portable/renode)
+  export PYRENODE_RUNTIME=coreclr
+  export PATH=$PATH:$(realpath renode_portable/)
+fi
+
+if [ -d /opt/renode ]; then
+  export PYRENODE_PATH=/opt/renode
+fi
